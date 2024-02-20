a9febd97f91613e41d8a9cb77041319397f6a60b
Nour-Cherrat <cherrat.nour@gmail.com>
2024-02-20 23:05:07 +0100

    test airbnb clone

--- models/base_model.py
+++ models/base_model.py
@@ -1,26 +1,51 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
+
 import uuid
 from datetime import datetime
+from sqlalchemy import Column, String, DATETIME
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), nullable=False, primary_key=True, unique=True)
+    created_at = Column(DATETIME, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DATETIME, nullable=False, default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
+        """
+        Instantiates a new model with the following attributes:
+
+        id: string - assign with an uuid when an instance is created
+
+        created_at: datetime - assign with the current datetime when an
+        instance is created and it will be updated every time the object
+        is changed.
+
+        updated_at: datetime - assign with the current datetime when an
+        instance is created and it will be updated every time the object
+        is changed.
+        """
+
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+            self.created_at = self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ('created_at', 'updated_at'):
+                        setattr(self, key, datetime.fromisoformat(value))
+                    else:
+                        setattr(self, key, value)
+            if not hasattr(kwargs, 'id'):
+                setattr(self, 'id', str(uuid.uuid4()))
+            if not hasattr(kwargs, 'created_at'):
+                setattr(self, 'created_at', datetime.now())
+            if not hasattr(kwargs, 'updated_at'):
+                setattr(self, 'updated_at', datetime.now())
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -28,17 +53,33 @@ class BaseModel:
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
-        """Updates updated_at with current time when instance is changed"""
+        """
+        Updates the public instance attribute updated_at with the current
+        datetime
+        """
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
-        """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
+        """
+        Returns a dictionary containing all keys/values of __dict__ of the
+        instance
+        """
+        new_dict = {}
+        for key, value in self.__dict__.items():
+            if key != '_sa_instance_state':
+                if isinstance(value, datetime):
+                    new_dict[key] = value.isoformat()
+                else:
+                    new_dict[key] = value
+        new_dict['__class__'] = type(self).__name__
+        return new_dict
+
+    def delete(self):
+        """
+        Deletes the current instance from the storage (models.storage)
+        """
+        from models import storage
+        storage.delete(self)
--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,31 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+"""
+This module instantiates an object of storage engine
+"""
+import os
+from tests.test_models.test_base_model import TestBasemodel
 from models.amenity import Amenity
 
 
-class test_Amenity(test_basemodel):
-    """ """
+class TestAmenity(TestBasemodel):
+    """
+    Test the Amenity class and its methods
+    """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """
+        Initializes the test class for Amenity
+        """
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
     def test_name2(self):
-        """ """
+        """
+        Tests the type of name attribute
+        """
         new = self.value()
-        self.assertEqual(type(new.name), str)
+        if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+            self.assertNotEqual(type(new.name), str)
+        else:
+            self.assertEqual(type(new.name), type(None))
